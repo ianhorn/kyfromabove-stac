feb51b57334611a748babb6a656c8aa425d7ed03
ianhorn <ian.horn@ky.gov>
2025-06-19 12:54:17 -0400

    initiate source that leverage titiler

---
+++ src/kyfromabove-stac/stac_item_titiler.py
@@ -0,0 +1,10 @@
+"""
+This python script creates a function that will be used to populate
+stac-item variables.  
+
+It will then use the titiler.extension to create a stac item
+"""
+
+import titiler 
+from titiler.extensions import stac
+
