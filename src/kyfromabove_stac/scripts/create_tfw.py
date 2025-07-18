cf344a6a8e0f61cb9e6e759eb57a02413770c59e
ianhorn <ian.horn@ky.gov>
2025-07-18 17:29:46 -0400

    edit

--- src/kyfromabove_stac/scripts/create_tfw.py
+++ src/kyfromabove_stac/scripts/create_tfw.py
@@ -12,24 +12,24 @@ tifs = pd.read_csv(csv)
 def create_tfw(tif):
 
 
-    # Open the dataset
-    with rasterio.open(tif) as dataset:
-        if os.path.exists(output_tfw):
-            transform = dataset.transform
+    # # Open the dataset
+    # with rasterio.open(tif) as dataset:
+    #     if os.path.exists(output_tfw):
+    #         transform = dataset.transform
 
-            # Extract GeoTransform values
-            tfw_values = [
-                transform.a,     # pixel width
-                transform.b,     # row rotation (typically zero)
-                transform.d,     # column rotation (typically zero)
-                transform.e,     # pixel height (usually negative)
-                transform.c,     # x-coordinate of center of upper-left pixel
-                transform.f      # y-coordinate of center of upper-left pixel
-            ]
+    #         # Extract GeoTransform values
+    #         tfw_values = [
+    #             transform.a,     # pixel width
+    #             transform.b,     # row rotation (typically zero)
+    #             transform.d,     # column rotation (typically zero)
+    #             transform.e,     # pixel height (usually negative)
+    #             transform.c,     # x-coordinate of center of upper-left pixel
+    #             transform.f      # y-coordinate of center of upper-left pixel
+    #         ]
 
-            # Write values to a .tfw file
-            with open(output_tfw, 'w') as f:
-                for value in tfw_values:
-                    f.write(f"{value}\n")
+    #         # Write values to a .tfw file
+    #         with open(output_tfw, 'w') as f:
+    #             for value in tfw_values:
+    #                 f.write(f"{value}\n")
 
-    print(f".tfw file created: {output_tfw}")
+    # print(f".tfw file created: {output_tfw}")
